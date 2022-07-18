56d3ea575d4f88db7c231aa59884ec699959309c
Tulyakai <tulyawatt@gmail.com>
2022-07-18 15:47:20 +0700

    feat: update authService

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,19 +1,42 @@
-import apiClient from "./AxiosClient";
+import apiClient from './AxiosClient'
 
 export default {
-    register(data) {
-        return apiClient.post('/auth/register/', data);
-    },
-    login(data) {
-        return apiClient.post('/auth/login/', data);
-    },
-    checkEmailExist(data) {
-        return apiClient.post('/auth/check-email/', data);
-    },
-    updateUserById(data) {
-        return apiClient.post('/auth/update/', data);
-    },
-    updatePasswordByUserId(data) {
-        return apiClient.post('/auth/update-password/', data);
-    }
+  register(data) {
+    return apiClient.post('/auth/register/', data)
+  },
+  login(data) {
+    return apiClient
+      .post('/auth/login/', data)
+      .then((res) => {
+        localStorage.setItem('token', res.data.token)
+        localStorage.setItem('user', JSON.stringify(res.data.user))
+        localStorage.setItem('role', res.data.user.role)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  },
+  logout() {
+    localStorage.removeItem('token')
+    localStorage.removeItem('user')
+    localStorage.removeItem('role')
+  },
+  checkEmailExist(data) {
+    return apiClient.post('/auth/check-email/', data)
+  },
+  updateUserById(data) {
+    return apiClient.post('/auth/update/', data)
+  },
+  updatePasswordByUserId(data) {
+    return apiClient.post('/auth/update-password/', data)
+  },
+  uploadFile(file) {
+    let formData = new FormData()
+    formData.append('file', file)
+    return apiClient.post('/uploadFile', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+  }
 }
